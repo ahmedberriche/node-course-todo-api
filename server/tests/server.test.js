8ee3678bf989c3b1f4ae536d7b292efda2888839
Ahmed <ahmedberriche1993@gmail.com>
2018-11-14 10:17:42 +0100

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -291,3 +291,25 @@ request(app)
 })
   });
 });
+
+describe('DELETE /users/me/token',()=>{
+  it('should remove auth token logout', (done)=>{
+    //delete /users/me/tokeen
+    //set x-auth equal to token
+    //200
+    //find user, verify that tokens array has length of 0
+    request(app)
+    .delete('/users/me/token')
+    .set('x-auth', users[0].tokens[0].token)
+    .expect(200)
+    .end((err, res) =>{
+      if(err){
+        return done(err);
+      }
+      User.findById(users[0]._id).then((user) =>{
+        expect(user.tokens.length).toBe(0);
+        done();
+      }).catch((e) => done(e));
+    })
+      });
+  });
